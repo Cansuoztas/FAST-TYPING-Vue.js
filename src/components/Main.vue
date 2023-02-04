65df40e73f488cfeeb870ffe9efb4768f27b6a44
Cansuoztas <fcansuoztas@gmail.com>
2023-02-04 21:11:59 +0300

    first

---
+++ src/components/Main.vue
@@ -0,0 +1,32 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+ <div class="container jumbotron">
+  <h1 class="display-4">Hızlı Yazma Yarışması</h1>
+  <p class="lead">Ne kadar hızlı klavye kullandığını test et...</p>
+  <hr class="my-4">
+
+  <div class="card">
+    <div class="card-body">
+        Elma Armut Kestane Kiraz
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-body">
+        <div class="input-group-lg">
+  <input type="text" class="form-control" >
+  <div class="input-group-append" id="button-addon4">
+    <button class="btn btn-outline-secondary" type="button">Button</button>
+    <button class="btn btn-outline-secondary" type="button">Button</button>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
