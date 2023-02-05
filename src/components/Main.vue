363ff53bf80831ba22000f1db3f031ca1bdecc39
Cansuoztas <fcansuoztas@gmail.com>
2023-02-05 23:10:23 +0300

    finish

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,18 +3,20 @@
  <div class="container jumbotron">
   <h1 class="display-4">Hızlı Yazma Yarışması</h1>
   <p class="lead">Ne kadar hızlı klavye kullandığını test et...</p>
-   <div>
-    Doğru Sayısı : {{ trueCount  }}
-    Yanlış Sayısı : {{ falseCount }}
-   </div>
   <hr class="my-4">
    <div v-if="isFinish" class="alert alert-primary">
-    <h1>Oyun Bitti</h1>
+    <h3>Süre Bitti</h3>
+    <p class="display-4">{{dks}}</p>
+    <span>Doğruluk Yüzdesi : % {{ truePercent }} </span>
+    <span>Doğru Kelime : {{trueCount}} </span>
+    <span>Yanlış Kelime : {{ falseCount }} </span>
+
+    <button @click="newGame" class="btn btn-success btn-lg mt-5 btn-block"> YENİ OYUN </button>
    </div>
    <div v-else>
   <div  class="card">
     <div class="card-body">
-        <span v-for="(word,key) in words" :key="key" v-bind:class="key!=0 || writingWordControl " class=" words m-2 " >{{word}}
+        <span v-for="(word,key) in words.filter((data,index)=>index<20)" :key="key" v-bind:class="key!=0 || writingWordControl " class=" words m-2 " >{{word}}
         </span>
     </div>
   </div>
@@ -24,7 +26,7 @@
       <input type="text" class="form-control" v-model="writingWord" >
   <div class="input-group-append" id="button-addon4">
     <button class="btn btn-light disabled" type="button">{{timer}}sn.</button>
-    <button class="btn btn-light" type="button">Yenile</button>
+    <button :disabled="isRunning" class="btn btn-light" type="button" @click="getWords">Yenile</button>
   </div>
 </div>
     </div>
@@ -43,7 +45,7 @@ data() {
     isTrue: true,
     trueCount:0,
     falseCount: 0,
-    timer : 5,
+    timer : 60,
     interval:false,
     isRunning:false,
     isFinish: false,
@@ -52,29 +54,55 @@ data() {
 },
 watch:{
   writingWord(val){
-
+    if(!val || val=== ' '){
+        this.writingWord=''
+        return
+    }
     if(!this.isRunning) this.toggleTimer()
         const word = this.words[0].slice(0, val.length)
-        let userWord = val.replace(' ','') //boşluğu sil
+        const userWord = val.replace(' ','') //boşluğu sil
+        //  const word = this.words[0].slice(0, val.length).toUpperCase()
+        //  const userWord = val.replace(' ','') .toUpperCase() //büyük-küçük harf
+
           this.isTrue = word === userWord
         if(val.indexOf(' ') !== -1){
           this.isTrue ? this.trueCount++ : this.falseCount++ 
           this.words.splice(0,1)
           this.writingWord=''
-
-
+          this.isTrue=true
         }
   }
 },
 computed:{
   writingWordControl(){
     return this.isTrue ? 'writing-word' : 'writing-word bg-danger'
+  },
+  dks (){
+    return 300 - this.words.length
+  },
+  truePercent(){
+    let percent =  (100/this.dks)
+    const val =  (percent * this.trueCount)
+    return isNaN(val) ? 0 : val
   }
 },
+
+
 mounted(){
   this.getWords()
 },
+
+
 methods:{
+
+  newGame(){
+    this.getWords()
+    this.isFinish=false
+    this.timer=60
+    this.isTrue=true
+    this.isRunning=false
+  },
+
   getWords(){
 this.words = this.wordList.sort(()=>Math.random()-0.5).splice(0,300)
   },
